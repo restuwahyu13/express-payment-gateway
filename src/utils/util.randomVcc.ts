f5f8b26b0ab86128d494c45e1daba9110d8b6c8d
restuwahyu13 <restuwahyu13@gmail.com>
2021-01-02 17:18:26 +0700

    AddFakeCC

--- src/utils/util.randomVcc.ts
+++ src/utils/util.randomVcc.ts
@@ -1,5 +1,6 @@
 export const randomVCC = (): number => {
 	const cc = 4728398706189983
 	const randomVirtualCreditCard = Math.random(cc).toString().replace('0.', '')
-	return randomVirtualCreditCard
+	const visaCreditCard = 4 + randomVirtualCreditCard
+	return +visaCreditCard
 }
